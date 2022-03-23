3bc77e280fd1ab448af0a065855ca55221a1dbdc
Marko Milijanovic <markomilijanovic01@gmail.com>
2022-03-23 14:59:28 +0100

    Second Commit

--- Kalkulator/Kalkulator/Program.cs
+++ Kalkulator/Kalkulator/Program.cs
@@ -5,6 +5,12 @@ while (true)
     int a, b;
     string[] input = Console.ReadLine().Split();
     if (input[0] == "kraj" || input[0] == "exit") break;
+    if(input[0] == "new")
+    {
+        string temp = Console.ReadLine();
+        Console.WriteLine(temp);
+        continue;
+    }
     a = int.Parse(input[0]);
     b = int.Parse(input[2]);
     switch (input[1])
