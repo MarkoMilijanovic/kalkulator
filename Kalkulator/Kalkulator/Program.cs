2335e2c6a9c84c4db581325de5ba6d62518c9d09
Marko Milijanovic <markomilijanovic01@gmail.com>
2022-03-23 14:53:11 +0100

    initial commit

---
+++ Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    int a, b;
+    string[] input = Console.ReadLine().Split();
+    if (input[0] == "kraj" || input[0] == "exit") break;
+    a = int.Parse(input[0]);
+    b = int.Parse(input[2]);
+    switch (input[1])
+    {
+        case "+": Console.WriteLine(a + b); break;
+        case "-": Console.WriteLine(a - b); break;
+        case "*": Console.WriteLine(a * b); break;
+        case "/": Console.WriteLine(a / b); break;
+    }
+}
